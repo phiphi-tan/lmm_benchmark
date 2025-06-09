f49a2950f0703b0a140abff362afbb06220a6623
Tan Phinehas Faithful (IA) <tfaithfu@dso.org.sg>
2025-06-09 15:16:22 +0800

    Add basic testing scripts

---
+++ llm_benchmark/hf_llavaov05b_OCR.py
@@ -0,0 +1,36 @@
+# https://huggingface.co/llava-hf/llava-onevision-qwen2-0.5b-ov-hf
+# "C:\Users\Tfaithfu\AppData\Local\Programs\Python\Python313\python.exe"
+# https://huggingface.co/datasets/TrainingDataPro/crowd-counting-dataset
+from transformers import pipeline
+from datasets import load_dataset
+import evaluate
+exact_match_metric = evaluate.load("exact_match")
+
+ds = load_dataset("MiXaiLL76/TextOCR_OCR", split='train')
+shuffled_ds = ds.shuffle()
+
+num_samples_to_process = 10
+ds_subset = shuffled_ds.select(range(num_samples_to_process))
+print(ds_subset)
+prompt = "You are an optical character recognition tool. Your task is process the text given in the attached image."\
+"Analyze the image and respond with the text shown. Do not provide any explanation or introductory text, or anything other than what is shown."
+
+pipe = pipeline("image-text-to-text", model="llava-hf/llava-onevision-qwen2-0.5b-ov-hf")
+prediction_list = [None for _ in range(num_samples_to_process)]
+reference_list = ds_subset['text']
+for i, entry in enumerate(ds_subset):
+    messages = [
+        {"role": "user",
+        "content": [{"type": "image"}, {"type": "text", "text": prompt}]},
+    ]
+    output = pipe(images=entry['image'],
+                  text=messages,
+                  generate_kwargs={"max_new_tokens": 20},
+                  return_full_text=False)
+    prediction_list[i] = output[0]['generated_text']
+    
+print("Predicted values: {}".format(prediction_list))
+print("Actual values: {}".format(reference_list))
+
+results = exact_match_metric.compute(references=reference_list, predictions=prediction_list)
+print("Exact Match Rate: {}".format(round(results["exact_match"], 2)))
---
+++ llm_benchmark/hf_llavaov05b_crowdcount.py
@@ -0,0 +1,33 @@
+# https://huggingface.co/llava-hf/llava-onevision-qwen2-0.5b-ov-hf
+# "C:\Users\Tfaithfu\AppData\Local\Programs\Python\Python313\python.exe"
+# https://huggingface.co/datasets/TrainingDataPro/crowd-counting-dataset
+from transformers import pipeline
+from datasets import load_dataset
+
+ds = load_dataset("TrainingDataPro/crowd-counting-dataset", split='train')
+shuffled_ds = ds.shuffle()
+
+num_samples_to_process = 5
+ds_subset = shuffled_ds.select(range(num_samples_to_process))
+print(ds_subset)
+prompt = "You are an image analysis tool. Your task is to estimate the crowd size in the provided image."\
+"Analyze the image and respond with a single number. Do not provide any explanation or introductory text, or any additional symbols or units of measurements."
+
+
+pipe = pipeline("image-text-to-text", model="llava-hf/llava-onevision-qwen2-0.5b-ov-hf")
+output_list = [None for _ in range(num_samples_to_process)]
+print(output_list)
+
+for i, entry in enumerate(ds_subset):
+    messages = [
+        {"role": "user",
+        "content": [{"type": "image"}, {"type": "text", "text": prompt}]},
+    ]
+    output = pipe(images=entry['image'],
+                  text=messages,
+                  generate_kwargs={"max_new_tokens": 20},
+                  return_full_text=False)
+    output_list[i] = output[0]['generated_text']
+    
+print("Predicted values: {}".format(output_list))
+print("Actual values: {}".format(ds_subset['label']))
